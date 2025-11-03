885c0ab191fcc042c9caee723ea557fa6f1ac6d0
lzh <l.zhonghua@outlook.com>
2025-11-03 16:00:32 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import { Hono } from "hono";
+import { Hono, type Context } from "hono";
 import { contentRoutes } from "./routes/roure.ts";
 import { staticRoutes } from "./routes/staticRouter.ts";
 import { cors } from "hono/cors";
+import { bearerAuth } from "hono/bearer-auth";
 
 const app = new Hono();
 
@@ -11,12 +12,31 @@ app.get("/", (c) => {
 
 app.route("/content", contentRoutes);
 app.route('', staticRoutes);
+//跨域配置
 app.use('/*', cors({
   origin:'https://note.lzhpro.top',
   allowMethods: ['GET', 'POST', 'OPTIONS'], // 允许的 HTTP 方法
   allowHeaders: ['Content-Type', 'Authorization'], // 允许的请求头
   maxAge: 3600, // 预检请求（Preflight Request）的缓存时间（秒）
 }));
+//验证token
+const verifyMyToken = async (token: string, c: Context): Promise<boolean> => {
+  // 示例：在这里查询数据库或缓存来验证 Token 是否有效
+  if (token === 'VALID_API_KEY_12345') {
+    // 可以在这里将用户信息设置到 Context 中，供后续路由使用
+    c.set('userId', 'user_abc');
+    return true; // 验证成功
+  }
+  return false; // 验证失败
+};
+//验证信息
+app.use('/*',
+  bearerAuth({
+    verifyToken: verifyMyToken,
+    prefix:'token'
+  })
+);
+//'Authorization': `token ${userToken}`
 app.notFound((c) => {
   return c.text('Not Found', 404);
 });
